dc16945c624ffabb1e2741590f15bccb6fa417d2
Markab-1 <velichko.anna.b@gmail.com>
2022-01-23 16:41:46 +0200

    bug-fixes-2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,31 +1,28 @@
 import throttle from 'lodash.throttle';
-const refs = {
-    form: document.querySelector(".feedback-form"),
-}
+
+const formRef = document.querySelector(".feedback-form");
 
 let data = {
     email: "",
-    message: "",
+    message: ""
 }
 
 const STORAGE_KEY = "feedback-form-state";
 
-refs.form.addEventListener("submit", onFormSubmit);
-refs.form.addEventListener("input", throttle(onFormInput, 500));
+formRef.addEventListener("submit", onFormSubmit);
+formRef.addEventListener("input", throttle(onFormInput, 500));
 
 fillingTextarea();
 
 function onFormInput(event) {
-    data.message = refs.form.elements.message.value;
-    data.email = refs.form.elements.email.value;
+    if (event.target.name === "email") { data.email = event.target.value; }
+    if (event.target.name === "message") { data.message = event.target.value; }
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 }
 
 function onFormSubmit(event) {
     event.preventDefault();
-    data.message = refs.form.elements.message.value;
-    data.email = refs.form.elements.email.value;
 
     if (!data.message || !data.email) {
         alert("Все поля должны быть заполнены");
@@ -34,14 +31,18 @@ function onFormSubmit(event) {
 
     console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
     event.target.reset();
-    localStorage.removeItem(STORAGE_KEY);    
+    localStorage.removeItem(STORAGE_KEY);
+    data.email = "";
+    data.message = "";
 }
 
 function fillingTextarea() {
-    const savedMsg = localStorage.getItem(STORAGE_KEY);
+    const savedMsg = JSON.parse(localStorage.getItem(STORAGE_KEY));
    
     if (savedMsg) {
-        refs.form.elements.message.value = JSON.parse(savedMsg).message; 
-        refs.form.elements.email.value = JSON.parse(savedMsg).email;
+        formRef.elements.message.value = savedMsg.message; 
+        formRef.elements.email.value = savedMsg.email;
+        data.message = savedMsg.message;
+        data.email = savedMsg.email;
     }
 }
